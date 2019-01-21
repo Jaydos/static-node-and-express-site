6f36a4f2d2643f757908d0bf3eb819aa07c86bc5
Jaydos <jaydenoxley00@gmail.com>
2019-01-21 20:30:29 +1000

    Added error handling. Working on elevator pitch and display pictures. Styling to come.

--- app.js
+++ app.js
@@ -17,10 +17,28 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/project/:id', (req, res) => {
+app.get('/project/:id', (req, res, next) => {
 
+    if (parseInt(req.params.id) > data.projects.length - 1){
+        const err = new Error('Invalid URL...');
+        err.status = 404;
+        return next(err);
+    }
     res.render('project', {id: parseInt(req.params.id), data: data.projects});
+});
+
+app.use((req, res, next) => {
+    const err = new Error('Woops, something went wrong...');
+    err.status = 404;
+    next(err);
 })
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
+
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
